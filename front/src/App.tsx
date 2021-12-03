b61bacf4ec483504b143170ea5990da5bf9989f7
minje70 <minje70@naver.com>
2021-12-03 16:12:00 +0900

    init

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+const DivStyle = styled.div`
+  font-size: 100px;
+`;
+
+function App() {
+  return <DivStyle>형태형 바보</DivStyle>;
+}
+
+export default App;
