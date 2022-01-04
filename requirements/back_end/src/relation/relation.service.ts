d1bd2f216096e3d02bc770f11c90934ad65e7d8f
Likilee <gnsdlrl@daum.net>
2022-01-04 16:40:48 +0900

    [added]relation user ID 받아서 항상 first로 간주하여 관계 리턴하는 리졸버 추가

--- requirements/back_end/src/relation/relation.service.ts
+++ requirements/back_end/src/relation/relation.service.ts
@@ -48,18 +48,53 @@ export class RelationService {
 
   sortRelationArgs(relation: RelationArgs): RelationArgs {
     if (relation.user_first_id > relation.user_second_id) {
-      const temp = relation.user_first_id;
-      relation.user_first_id = relation.user_second_id;
-      relation.user_second_id = temp;
-
-      //RE0 or RE1 이었으면 RE1 or RE0 이 되도록 한다.
-      if (relation.typeId === 'RE0' || relation.typeId === 'RE1') {
-        relation.typeId = relation.typeId === 'RE0' ? 'RE1' : 'RE0';
-        //RE3 or RE4 이었으면 RE4 or RE3 이 되도록 한다.
-      } else if (relation.typeId === 'RE3' || relation.typeId === 'RE4') {
-        relation.typeId = relation.typeId === 'RE3' ? 'RE4' : 'RE3';
-      }
+      this.switchRelation(relation);
     }
+    return relation;
+  }
+
+  async getRelationsByUserIdTreatAsFirst(
+    userId: number,
+    typeId?: string,
+  ): Promise<Relation[]> {
+    const relations = await this.findRelationByUserId(userId);
+    const switched = this.switchRelationByUserIdTreatAsFirst(relations, userId);
+
+    if (typeId) {
+      return this.filterRelationByTypeId(switched, typeId);
+    }
+    return switched;
+  }
+
+  filterRelationByTypeId(relations: Relation[], typeId: string): Relation[] {
+    return relations.filter((relation) => {
+      if (relation.typeId && relation.typeId === typeId) return true;
+    });
+  }
+
+  switchRelationByUserIdTreatAsFirst(relations: Relation[], userId: number) {
+    const switched = relations.map((relation) => {
+      if (relation.user_second_id === userId) {
+        relation = this.switchRelation(relation) as Relation;
+      }
+      return relation;
+    });
+    return switched;
+  }
+
+  switchRelation(relation: RelationArgs): RelationArgs {
+    const temp = relation.user_first_id;
+    relation.user_first_id = relation.user_second_id;
+    relation.user_second_id = temp;
+
+    //RE0 or RE1 이었으면 RE1 or RE0 이 되도록 한다.
+    if (relation.typeId === 'RE0' || relation.typeId === 'RE1') {
+      relation.typeId = relation.typeId === 'RE0' ? 'RE1' : 'RE0';
+      //RE3 or RE4 이었으면 RE4 or RE3 이 되도록 한다.
+    } else if (relation.typeId === 'RE3' || relation.typeId === 'RE4') {
+      relation.typeId = relation.typeId === 'RE3' ? 'RE4' : 'RE3';
+    }
+
     return relation;
   }
 }
