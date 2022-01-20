a55a2372d8075212809e309bbc73028915373eee
hyongti <hyongtiii@gmail.com>
2022-01-20 16:15:06 +0900

    [added]로그인 화면으로 갔을 때 소켓 연결 해제

--- requirements/front_end/src/routes/LoginPage.tsx
+++ requirements/front_end/src/routes/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import BackBoard from '../components/common/BackBoard';
 import Ball from '../components/common/Ball';
 import Body, {
@@ -6,11 +6,17 @@ import Body, {
   BackboardBoxLayout,
 } from '../components/common/Body';
 import Button from '../components/common/Button';
+import {GameData} from '../components/pongGame/GameData';
 
 export default function LoginPage(): JSX.Element {
   const onClick = () => {
     window.location.href = 'http://api.ts.io:30000/auth/fortytwo';
   };
+  useEffect(() => {
+    if (GameData.socket) {
+      GameData.socket.disconnect();
+    }
+  }, []);
   return (
     <>
       <Ball />
